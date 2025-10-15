cb502eb28fdf9030db3cd7f1b4d10e3a017e163e
Maximilian Jacobi <maximilian.jacobi@uni-jena.de>
2025-10-15 11:48:12 +0200

    simple animation example

---
+++ examples/simple_anim.py
@@ -0,0 +1,92 @@
+import argparse
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+import yaaps as ya
+import yaaps.plot2D as yp
+
+ap = argparse.ArgumentParser("Make an animation and save the frames as png")
+
+ap.add_argument('var', type=str, help="Variable to plot")
+ap.add_argument('-o','--outputpath', type=str, default=None,
+                help="Path to save at")
+ap.add_argument('-s','--simdir', type=str, default='active',
+                help="Directory to look for athdf files in")
+ap.add_argument('-r','--sampling', type=str, default='xy',
+                help="Plane to plot")
+ap.add_argument('-b', '--boundary', type=float, default=None,
+                help="Boundary of the plot")
+ap.add_argument('--time_min', type=float, default=None,
+                help="Time to start at")
+ap.add_argument('--time_max', type=float, default=None,
+                help="Time to end at")
+ap.add_argument('--time_every', type=int, default=1,
+                help="Create frames at every nth output time")
+ap.add_argument('-m','--meshblocks', action="store_true",
+                help="Draw mesh-block boundaries")
+ap.add_argument('-f','--func', default="None", type=str,
+                help="Modify plot with given function. (calls eval)")
+ap.add_argument('-c','--cmap', type=str, default=None,
+                help="Colormap")
+ap.add_argument('-n','--norm', type=str, default=None,
+                help="-n 'log' for logsscale")
+ap.add_argument('--vmin', type=float, default=None,
+                help="Minimum of the colorscale")
+ap.add_argument('--vmax', type=float, default=None,
+                help="Maximum of the colorscale")
+
+if len(sys.argv) == 1:
+    sim = ya.Simulation("active")
+    av_v = sorted(list(set(vv for vv, *_ in sim.scrape.debug_data_keys().keys())))
+    print("Available vars:")
+    for vv in av_v:
+        print(f"  {vv}")
+    exit(0)
+
+args = ap.parse_args()
+
+sim = ya.Simulation(args.simdir)
+func = eval(args.func)
+
+fig, ax = plt.subplots(1, figsize=(6, 4), animated=True)
+
+ax.set_xlim(-args.boundary, args.boundary)
+ax.set_ylim(-args.boundary, args.boundary)
+
+kwargs = dict(
+    var=args.var,
+    norm=args.norm,
+    func=func,
+    sampling=args.sampling,
+    cmap=args.cmap,
+    draw_meshblocks=args.meshblocks,
+    vmin=args.vmin,
+    vmax=args.vmax,
+    ax=ax,
+    )
+
+for k, v in list(kwargs.items()):
+    if v is None:
+        kwargs.pop(k)
+
+plot = yp.NativeColorPlot(sim=sim, **kwargs)
+
+times = plot.time_range
+if args.time_min is not None:
+    times = times[times>=args.time_min]
+if args.time_max is not None:
+    times = times[times<=args.time_max]
+times = times[::args.time_every]
+    
+frames = yp.save_frames(
+        times=times,
+        fig=fig,
+        plots=[plot],
+        output_dir=f"{args.var}_{args.sampling}",
+        prefix=f"frame_",
+        dpi=300,
+        )
+
+plt.close()
+
